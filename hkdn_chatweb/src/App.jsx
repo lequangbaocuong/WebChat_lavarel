e59a39d2bf99a2442b0aa2ffab31bad9c332f100
Vantienhuynh <Vantienhuynh2105@gmail.com>
2024-11-01 19:31:24 +0700

    Tao_moi_truong

---
+++ hkdn_chatweb/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <header>
+            <h1>Welcome to My React Demo!</h1>
+            <p>This is a simple React application for testing purposes.</p>
+        </header>
+    );
+}
+
+export default App;
